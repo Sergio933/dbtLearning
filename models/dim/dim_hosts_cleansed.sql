6bb142d7b43df164101a9f361f105f49c9307f0e
Sergio Scandurra <scandurrasergio93@gmail.com>
2024-04-11 16:50:23 +0200

    test

---
+++ models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,10 @@
+WITH src_hosts AS (
+   SELECT * FROM {{ref("src_hosts")}} 
+)
+SELECT 
+    HOST_ID
+    ,IFNULL(HOST_NAME,'Anonymous') AS host_name
+    ,IS_SUPERHOST
+    ,CREATED_AT
+    ,UPDATED_AT
+FROM src_hosts
